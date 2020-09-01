092ebe87eb93d1351f767daedf84a492e439c256
nirentuladhar <nirentuladhar@gmail.com>
2020-09-01 13:48:26 +1000

    Github auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,74 @@
+import App from 'next/app'
 import '../styles/global.scss'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
-import { TinaProvider, TinaCMS } from 'tinacms'
+import { TinaCMS, TinaProvider } from 'tinacms'
+import { GithubClient, TinacmsGithubProvider } from 'react-tinacms-github'
 
-function MyApp ({ Component, pageProps }) {
-  const cms = new TinaCMS({ enabled: true, sidebar: true })
-  return (
-    <>
-      <TinaProvider cms={cms}>
-        <Nav />
-        <Component {...pageProps} />
-        <Footer />
+export default class Site extends App {
+  constructor (props) {
+    super(props)
+
+    this.cms = new TinaCMS({
+      enabled: !!props.pageProps.preview,
+      apis: {
+        github: new GithubClient({
+          proxy: '/api/proxy-github',
+          authCallbackRoute: '/api/create-github-access-token',
+          clientId: process.env.GITHUB_CLIENT_ID,
+          baseRepoFullName: process.env.REPO_FULL_NAME
+        })
+      },
+
+      sidebar: props.pageProps.preview,
+      toolbar: props.pageProps.preview
+    })
+  }
+
+  render () {
+    const { Component, pageProps } = this.props
+    return (
+      <TinaProvider cms={this.cms}>
+        <TinacmsGithubProvider
+          onLogin={onLogin}
+          onLogout={onLogout}
+          error={pageProps.error}
+        >
+          <EditLink cms={this.cms} />
+          <Nav />
+          <Component {...pageProps} />
+          <Footer />
+        </TinacmsGithubProvider>
       </TinaProvider>
-    </>
-  )
+    )
+  }
 }
 
-export default MyApp
+const onLogin = async () => {
+  const token = localStorage.getItem('tinacms-github-token') || null
+  const headers = new Headers()
+
+  if (token) {
+    headers.append('Authorization', 'Bearer ' + token)
+  }
+
+  const resp = await fetch(`/api/preview`, { headers: headers })
+  const data = await resp.json()
+
+  if (resp.status == 200) window.location.href = window.location.pathname
+  else throw new Error(data.message)
+}
+
+const onLogout = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload()
+  })
+}
+
+export const EditLink = ({ cms }) => {
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? 'Exit Edit Mode' : 'Edit This Site'}
+    </button>
+  )
+}
